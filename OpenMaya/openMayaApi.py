f17a74a63aeac015c4611909e1fd9133a9f577d4
ZhongkaiLiu <zhongkailiu@tencent.com>
2024-10-25 18:27:35 +0800

    调整文件目录

---
+++ OpenMaya/openMayaApi.py
@@ -0,0 +1,5 @@
+import maya.OpenMaya as om
+
+# selection list
+mSelectionList = om.MSelectionList()
+mSelectionList.add("")
---
+++ main.py
@@ -0,0 +1,12 @@
+# _*_ coding: utf-8 _*_
+# @FileName:main.py
+# @Data:2024-10-25 : 18 : 24
+# @Author:zhongkailiu
+# @Contact:zhongkailiu@tencent.com
+'''
+launch:
+import main.py as my_FileName
+reload(my_FileName)
+my_FileName.main()
+'''
+print("hello")
